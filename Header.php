15d687bdacec0e30a0989ca80fc6bf9a9e5b33d4
vkapitanov <velko85@yahoo.com>
2013-09-25 12:42:47 +0300

    A
    parts

---
+++ Header.php
@@ -0,0 +1,12 @@
+<?php
+require 'A/articles.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?= $pageTitle;?></title>
+
+        <meta charset="UTF-8">       
+    </head>
+    <body>
